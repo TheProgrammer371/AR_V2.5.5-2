87ef63d435632c55b5f0bd0268da4133eef28516
TheProgrammer371 <144122452+TheProgrammer371@users.noreply.github.com>
2023-10-16 23:52:32 +0200

    Create Permissions.py

---
+++ Permissions.py
@@ -0,0 +1,61 @@
+LOGINS = {
+    "AppleSW": 66640966275310,
+	"Daiziro": 238300918622439,
+	"Fake": 75361033100300,
+	"Fake2": 163799953719,
+	"callmefixy": 929353418629,
+	"baffio00": 279321968880011,
+	"BassGhost": 9413546049668,
+	"DaddyCappy": 145665283325076, #145665283325076 173402165905993
+	"Sylence": 132127256221807,
+	"Daiiziro": 123719796879470,
+	"pogo_Frk": 220776870909600,#176156952522376 220776870909600
+	"Dream":171983375863228,
+	"Marko":136411542242152,
+	"x_fire2":273184161046606,
+	"saiyanevolution":17613682292892,
+	"Tony":123719803956179,
+}
+LOGINS_CREDENTIAL = {
+    "AppleSW": "Notem1537",
+	"Daiziro": "483dFmvhn4Af840sANcm19La23lkja",
+	"Fake": "Fakesublt2020",
+	"Fake2": "Fakesublt20202",
+	"callmefixy": "Ciales16",
+	"baffio00": "MelaRossa12",
+	"BassGhost": "BassNotGhost123",
+	"DaddyCappy": "ILoveApples",
+	"Sylence": "Sylence01",
+	"Daiiziro": "ilovechilds123",
+	"pogo_Frk": "1096juan",
+	"Dream":"1218",
+	"Marko":"321Legende",
+	"x_fire2":"alepale",
+	"saiyanevolution":"Filthymonkman123",
+	"Tony":"steak44",
+}
+
+ 
+LOG_INS = [66640966275310,
+238300918622439,
+75361033100300,
+929353418629,
+163799953719,
+279321968880011,
+9413546049668,
+173402165905993,
+132127256221807,
+123719796879470,
+220776870909600,
+145665283325076,
+176156952522376,
+171983375863228,
+136411542242152,
+273184161046606,
+17613682292892,
+123719803956179]
+
+OFFLINE = False
+WhatTheFlip = True
+TheFlipWhat = True
+ONEMODET = True
